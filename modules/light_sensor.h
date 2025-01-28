7e3d259e2dac5a29770de444819d3cd3ec0e991b
BaileyClark33 <91906664+BaileyClark33@users.noreply.github.com>
2025-01-28 15:32:26 -0500

    Create light_sensor.h

---
+++ modules/light_sensor.h
@@ -0,0 +1,16 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _LIGHT_SENSOR_H_
+#define _LIGHT_SENSOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void sensorUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _LIGHT_SENSOR_H_
