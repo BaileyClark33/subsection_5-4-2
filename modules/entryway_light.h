d98e3b4fdb4f457bdb957a11226fc02fc6c68c63
BaileyClark33 <91906664+BaileyClark33@users.noreply.github.com>
2025-01-28 15:31:04 -0500

    Create entryway_light.h

---
+++ modules/entryway_light.h
@@ -0,0 +1,16 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _ENTRYWAY_LIGHT_H_
+#define _ENTRYWAY_LIGHT_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void entryLight();
+
+//=====[#include guards - end]=================================================
+
+#endif // _ENTRYWAY_LIGHT_H_
